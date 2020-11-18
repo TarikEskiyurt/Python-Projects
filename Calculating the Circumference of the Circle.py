782b1879bec760c8f21edb73557ca18ec953fcc9
TarikEskiyurt <74590497+TarikEskiyurt@users.noreply.github.com>
2020-11-18 20:44:07 +0300

    Update Calculating the Circumference of the Circle.py

--- Calculating the Circumference of the Circle.py
+++ Calculating the Circumference of the Circle.py
@@ -1,10 +1,10 @@
-yrcp=int(input("Dairenizin yarıçapını giriniz"))
-cevre=2*3.14*yrcp
+yrcp=int(input("Dairenizin yarıçapını giriniz")) #kullanıcıya çevresini bulmak istediği çemberin yarıçapını soruyoruz
+cevre=2*3.14*yrcp #çevreyi hesaplıyoruz ve "cevre" değişkenine atıyoruz
 
 if cevre <= 0:
-    print("Pozitif değer giriniz")
+    print("Pozitif değer giriniz")    #yarıçap negatif bir değer ise pozitif değer girmesini istiyoruz
 
 if cevre == str:
-    print("Lütfen bir sayı giriniz")
+    print("Lütfen bir sayı giriniz")  #kullanıcı yarıçapa bir karakter girmişse, sadece sayı girmesini istiyoruz
 
-print("dairenin çevresi :",cevre)
+print("dairenin çevresi :",cevre) #dairenin çevresini ekrana yazdırıyoruz
