c68c609a9b0d6da3088b9068ed4f30a2ef94c673
TarikEskiyurt <74590497+TarikEskiyurt@users.noreply.github.com>
2020-11-18 20:47:52 +0300

    Update how many seconds did you live.py

--- how many seconds did you live.py
+++ how many seconds did you live.py
@@ -1,7 +1,8 @@
-from datetime import *
+from datetime import *  #doğum günü kütüphanesini projemize ekliyoruz
 
-birth = datetime.strptime(input("senin doğum günün(d.m.Y): "), "%d.%m.%Y")
+birth = datetime.strptime(input("senin doğum günün(d.m.Y): "), "%d.%m.%Y") #kullanıcıdan doğum tarihini girmesini istiyoruz
 
-age = datetime.now() - birth
+age = datetime.now() - birth #bügünkü tarihten kullanıcının doğum tarihini çıkarıyoruz
 
-print("EN=You lived {} seconds .".format(age.total_seconds()),"\nTR= {} saniye yaşadınız.".format(age.total_seconds()))
+print("EN=You lived {} seconds .".format(age.total_seconds()),"\nTR= {} saniye yaşadınız.".format(age.total_seconds())) #gün sayısını saniyeye çevirip ekrana yazdırıyoruz
+
