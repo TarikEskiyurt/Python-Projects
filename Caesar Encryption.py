c10b4215f04f7f2346c6c714a4033e1feda99c98
TarikEskiyurt <74590497+TarikEskiyurt@users.noreply.github.com>
2020-11-18 20:44:46 +0300

    Update Caesar Encryption.py

--- Caesar Encryption.py
+++ Caesar Encryption.py
@@ -1,11 +1,11 @@
-metin=input("Şifrelemek istediğiniz metni giriniz")
-artmik=int(input("Artma miktarı  ne olsun"))
+metin=input("Şifrelemek istediğiniz metni giriniz")  #kullanıcının şifrelemek istediği metni soruyoruz
+artmik=int(input("Artma miktarı  ne olsun")) #kullanıcıdan her bir rakamı kaç arttırarak metni şifrelemek istediğini soruyoruz
 sifre=""
 
-for k in metin:
+for k in metin:    #bütün rakamlara "artmik" teki değeri ekliyoruz ve şifrelenmiş metni "sifre" değişkenine atıyoruz
     print(ord(k))
     print(k, "=>", chr(ord(k) + artmik))
     sifre = sifre + chr(ord(k) + artmik)
 
-print(metin, " =>", sifre)
+print(metin, " =>", sifre) #şifrelenmiş metni ekrana yazdırıyoruz
 
