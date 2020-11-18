cd58a67b8981108d7fa8a1b2016e8296249cd1d1
TarikEskiyurt <74590497+TarikEskiyurt@users.noreply.github.com>
2020-11-18 20:46:23 +0300

    Update Number Guessing Game.py

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,28 +1,27 @@
-import random as r
+import random as r  #projemize random kütüphanesini ekliyoruz
 
-rastgele=r.randint(1,100)
+rastgele=r.randint(1,100)  #"rastgele" değişkenine 1 ile 100 arasında herhangi bir sayıyı atıyoruz
 
-tahmins=0
+tahmins=0   #"tahmins" değişkenine ilk olarak 0'ı atıyoruz
 
 
 
-sayi=int(input("1 ile 100 arasında bir sayı tuttum, bil bakalım kaç?"))
+sayi=int(input("1 ile 100 arasında bir sayı tuttum, bil bakalım kaç?"))  #kullanıcıdan sayıyı tahmin etmesini istiyoruz
 while True:
     
     if sayi > rastgele:
         tahmins = tahmins + 1
-        print("Daha küçük bir sayı giriniz")
+        print("Daha küçük bir sayı giriniz")  #kullanıcının girdiği sayı, rastgele tutulan sayıdan büyükse tekrar denemesini söylüyoruz
         sayi=int(input("----------------------------------\nBir daha tahmin et"))
     
 
     elif sayi < rastgele:
         tahmins = tahmins + 1
-        print("Daha büyük bir sayı giriniz")
+        print("Daha büyük bir sayı giriniz")  #kullanıcının girdiği sayı, rastgele tutulan sayıdan küçükse tekrar denemesini söylüyoruz
         sayi=int(input("----------------------------------\nBir daha tahmin et"))
 
     else:
         tahmins = tahmins + 1
-        print("Tebrikler, ",tahmins," seferde bildiniz...")
+        print("Tebrikler, ",tahmins," seferde bildiniz...")  #eğer cevap doğruysa kullanıcıyı tebrik edip programdan çıkış yapıyoruz
         break
-
 
