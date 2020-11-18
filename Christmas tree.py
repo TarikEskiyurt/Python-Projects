db6195482fb7c4009890d58c0006ca162be37699
TarikEskiyurt <74590497+TarikEskiyurt@users.noreply.github.com>
2020-11-18 20:43:10 +0300

    Update Christmas tree.py

--- Christmas tree.py
+++ Christmas tree.py
@@ -1,7 +1,7 @@
-height = int(input("noel ağacının yüksekliği: "))
+height = int(input("noel ağacının yüksekliği: ")) #kullanıcıdan noel ağacının yüksekliğini girmesini istiyoruz
 
 for i in range(int(height * 0.7)):
-    print( (" " * (height - (i // 2))) + ("*" * i))
+    print( (" " * (height - (i // 2))) + ("*" * i)) #noel ağacının yapraklarını ekrana yazdırıyoruz
 
 for i in range(int(height * 0.7), height):
-    print((" " * (height - 1)) + "||")
+    print((" " * (height - 1)) + "||")   #noel ağacının gövdesini ekrana yazdırıyoruz
