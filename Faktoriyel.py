db6364173975db7cf7aa3df69d3b075d050a7569
TarikEskiyurt <74590497+TarikEskiyurt@users.noreply.github.com>
2021-01-04 20:37:23 +0300

    Factorial Project
    I made a project that take the you want number's factorials. I hope you will like it...

---
+++ Faktoriyel.py
@@ -0,0 +1,18 @@
+a=int(input("Enter the number which number do you want to take factorial."))
+counter=a
+
+if a == 0:
+     print("1")
+
+else:
+     
+     while counter > 1:
+          counter = counter-1
+          a=a*(counter)
+     print(a)
+
+     if a == 0:
+          print("1")
+
+
+
