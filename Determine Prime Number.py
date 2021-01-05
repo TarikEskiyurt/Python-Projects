02407dc4131a91ed8bf24559bbbfa4d1fcd42eda
TarikEskiyurt <74590497+TarikEskiyurt@users.noreply.github.com>
2021-01-05 17:16:39 +0300

    Update Determine Prime Number.py

--- Determine Prime Number.py
+++ Determine Prime Number.py
@@ -1,11 +1,12 @@
-number=int(input("Please enter the number"))
+number=int(input("Please enter the number")) #I take a number from user
 counter=0
 
 for i in range(2,number):
      if number%i==0:
-          counter=counter+1
+          counter=counter+1 
 
 if counter > 0:
-     print("It is not a prime number.")
+     print("It is not a prime number.")  #I print outout to screen
 else:
-     print("Is the prime number.")
+     print("Is the prime number.")  #I print outout to screen
+
